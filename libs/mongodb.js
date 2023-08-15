9be5f5bdf62b3dc0e3f00ed6f83aa16229fa3ee9
Daniyal Saeed Dani <62589953+Dani-Bhai@users.noreply.github.com>
2023-08-15 10:05:37 +0500

    Update mongodb.js

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://danibhaialt:IPx53nek8kDHrkNz@cluster0.qsnrjir.mongodb.net/");
+    await mongoose.connect("mongodb+srv://<username>:<password>@cluster0.qsnrjir.mongodb.net/");
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
